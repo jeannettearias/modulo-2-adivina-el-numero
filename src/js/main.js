857d2e46f4eca5ab64dbe5e2e8a840989c5a0b47
jeannette <jeannettearias@gmail.com>
2024-06-11 22:08:10 +0200

    game logic finish, missing the look & feels

--- src/js/main.js
+++ src/js/main.js
@@ -5,16 +5,20 @@
 const inputValue = document.querySelector('.js__input');
 const btn = document.querySelector('.js__btn');
 const randomNumber = getRandomNumber(100);
+const hint = document.querySelector('.js__hint');
+const numberOfTries = document.querySelector('.js__numberOfTries');
+let count = 0;
+
 
 // GENERATE THE RANDOM NUMBER (1 - 100) TO BE GUESSED 
 
 function getRandomNumber(max) {
     return parseInt(Math.random() * max);
-   
 
-  }
 
-  // FUNCTIONS AND EVENTS
+}
+
+// FUNCTIONS AND EVENTS
 
 // GET THE VALUE INPUT BY USER 
 function getValueFromInput() {
@@ -25,49 +29,50 @@ function getValueFromInput() {
 }
 
 // print random number in console  
-function printRandomNumber (number) {
+function printRandomNumber(number) {
     console.log("Mi número aleatorio es " + number)
-} 
+}
 
 
 // comparison of number obtained vs input by user 
-function compareNumberUser (numberByUser, randomNumber) {
+function compareNumberUser(numberByUser, randomNumber) {
 
     console.log(numberByUser, randomNumber);
     if (numberByUser < 1 || numberByUser > 100) {
-        return "El número debe estar entre 1 y 100"
+        return "Pista: El número debe estar entre 1 y 100"
     }
 
     if (randomNumber === numberByUser) {
         return "Has ganado campeona!!!";
     }
     else if (randomNumber > numberByUser && numberByUser < 100) {
-        return "Demasiado bajo";
+        return "Pista: Demasiado bajo";
 
     }
     else if (randomNumber < numberByUser) {
-        return "Demasiado alto";
+        return "Pista: Demasiado alto";
     }
 }
 
-    //CLICK BUTTON FUNCTION 
-  const handleClickBtn = (ev) => {
+//CLICK BUTTON FUNCTION 
+const handleClickBtn = (ev) => {
     ev.preventDefault();
-
-    let numberByUser = getValueFromInput();
     
+    let numberByUser = getValueFromInput();
+
     printRandomNumber(randomNumber);
 
-    let game = compareNumberUser(numberByUser,randomNumber);
+    let game = compareNumberUser(numberByUser, randomNumber);
 
-    console.log(game);
+    // GENERATE ANSWERS TEXTS 
+    count += 1;
+    hint.innerHTML = `${game}`
+    numberOfTries.innerHTML = `Número de intentos: ${count}`
 
-  };
-  
-  btn.addEventListener( 'click', handleClickBtn );
+};
 
+btn.addEventListener('click', handleClickBtn);
 
-// GENERATE ANSWERS TEXTS 
 
 
 
